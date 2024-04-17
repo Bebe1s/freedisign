730dfaa6a1b3e67e744d521f856fb1c5b4fa053f
Bennett B <127968966+Bebe1s@users.noreply.github.com>
2024-04-17 08:30:38 -0400

    Update V1 Main.py

--- V1 Main.py
+++ V1 Main.py
@@ -2,24 +2,24 @@ import PySimpleGUI as sg
 import paramiko
 
 command = "df"
-host = "192.168.153.235" 
-username = "bennettb" 
-password = "12345678"
+host = "YOUR PI'S IP ADDRES HERE" 
+username = "YOUR PI'S USERNAME HERE" 
+password = "YOUR PASSWORD TO YOUR PI HERE"
 
 client = paramiko.client.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 client.connect(host, username=username, password=password)
 
-_stdin, _stdout,_stderr = client.exec_command("rm -fR Raiderweekly")
+_stdin, _stdout,_stderr = client.exec_command("rm -fR THE NAME OF YOUR REPOSITORY HERE")
 print(_stdout.read().decode())
 
-_stdin, _stdout,_stderr = client.exec_command("rm -fR /home/bennettb/.config/autostart/autovlc.desktop")
+_stdin, _stdout,_stderr = client.exec_command("rm -fR /home/YOUR USERNAME HERE/.config/autostart/autovlc.desktop")
 print(_stdout.read().decode())
 
-_stdin, _stdout,_stderr = client.exec_command("git clone https://github.com/Bebe1s/Raiderweekly.git")
+_stdin, _stdout,_stderr = client.exec_command("git clone https://github.com/GITHUB NAME HERE/THE NAME OF YOUR REPOSITORY HERE.git")
 print(_stdout.read().decode())
 
-_stdin, _stdout,_stderr = client.exec_command("mv /home/bennettb/Raiderweekly/autovlc.desktop /home/bennettb/.config/autostart")
+_stdin, _stdout,_stderr = client.exec_command("mv /home/YOUR USERNAME HERE/THE NAME OF YOUR REPOSITORY HERE/autovlc.desktop /home/YOUR USERNAME HERE/.config/autostart")
 print(_stdout.read().decode())
 
 _stdin, _stdout,_stderr = client.exec_command("sudo reboot")
